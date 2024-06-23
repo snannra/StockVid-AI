f4eedc6ad22d44058f5ac0e724addce364a4493b
Sohan Nannra <49798755+snannra@users.noreply.github.com>
2024-06-23 16:21:19 -0700

    added fonts

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Slot, Stack } from 'expo-router'
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false}}/>
+    </Stack>
+  )
+}
+
+export default RootLayout
+
+
+/*
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+*/
